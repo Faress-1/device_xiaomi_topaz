94165a606aa92ea0876ed9cc02d1a5441329a2e2
Faress-1 <149329824+Faress-1@users.noreply.github.com>
2024-06-27 23:28:01 +0300

    Update and rename bliss_topaz.mk to derp_topaz.mk

--- derp_topaz.mk
+++ derp_topaz.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
-PRODUCT_NAME := bliss_topaz
+PRODUCT_NAME := derp_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
