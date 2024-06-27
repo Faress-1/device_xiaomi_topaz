df3d2e323977723a20bcb4683ed79f3cd2491036
Faress-1 <149329824+Faress-1@users.noreply.github.com>
2024-06-27 21:10:01 +0300

    Update and rename superior_topaz.mk to bliss_topaz.mk

--- bliss_topaz.mk
+++ bliss_topaz.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from topaz device
 $(call inherit-product, device/xiaomi/topaz/device.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
-PRODUCT_NAME := superior_topaz
+PRODUCT_NAME := bliss_topaz
 PRODUCT_DEVICE := topaz
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
